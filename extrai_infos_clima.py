a1c6d29fb865750664f4f4561f6ecdfa65a35f7b
Arii19 <arianer19@otmail.com>
2025-02-24 20:06:39 -0300

    dags

--- extrai_infos_clima.py
+++ extrai_infos_clima.py
@@ -1,31 +1,42 @@
-import os
+from airflow import DAG 
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.macros import ds_add
+import pendulum
 from os.path import join
 import pandas as pd
-from datetime import datetime, timedelta
 
-# intervalo de datas
-data_inicio = datetime.today()
-data_fim = data_inicio + timedelta(days=7)
+with DAG(
+    "dados_climaticos",
+    start_date=pendulum.datetime(2025, 2, 22, tz="UTC"),
+    schedule_interval='0 0 * * 1', # executar toda segunda feira
+) as dag:
 
-# formatando as datas para pegar mês ano e dia
-data_inicio = data_inicio.strftime('%Y-%m-%d')
-data_fim = data_fim.strftime('%Y-%m-%d')
+    tarefa_1 = BashOperator(
+        task_id = 'cria_pasta',
+        bash_command = 'mkdir -p "/home/millenagena/Documents/airflowalura/semana={{data_interval_end.strftime("%Y-%m-%d")}}"'
+    )
 
-city = 'Adamantina'
-key = '7JCBNS69BDJQ4B7A6LZ8GRZP5'
+    def extrai_dados(data_interval_end):
+        city = 'Boston'
+        key = 'ANZQ5K8QQP8BXZ85F4ZEQ2FPK'
 
-URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
-    f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv') #definir parametros que os dados venham
+        URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+            f'{city}/{data_interval_end}/{ds_add(data_interval_end, 7)}?unitGroup=metric&include=days&key={key}&contentType=csv')
 
-#Declarando a variavel, em que os daods vem do url anterior
-dados = pd.read_csv(URL)
-print(dados.head())
+        dados = pd.read_csv(URL)
 
-#caminho para a geração do CSV - Aletrar se usar em outro PC
-file_path = r"C:\Users\Microsoft\Documents\PYTHON\extra-_dados_climaticos\semana02"
-os.mkdir(file_path)
+        file_path = f'/home/millenagena/Documents/airflowalura/semana={data_interval_end}/'
 
-dados.to_csv(file_path + 'dados_brutos.csv')
-dados[['datetime','tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
-dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
+        dados.to_csv(file_path + 'dados_brutos.csv')
+        dados[['datetime','tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
+        dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
+
+    tarefa_2 = PythonOperator(
+        task_id = 'extrai_dados',
+        python_callable = extrai_dados,
+        op_kwargs = {'data_interval_end': '{{data_interval_end.strftime("%Y-%m-%d")}}'}
+    )
+
+    tarefa_1 >> tarefa_2ile_path + 'condicoes.csv')
 
